7a720c14f17a6f6b0982cb63849c95682355fc27
RomanKudelin <76-767@mail.ru>
2024-02-05 13:33:27 +0300

    Решение для задачи неверное, перестраивает числа в зеркальном порядке

--- Csharp/task2/Program.cs
+++ Csharp/task2/Program.cs
@@ -29,11 +29,41 @@
 
 // ЗАДАЧА № 3
 
-{
-Console.Write("Введите число из отрезка [10, 99]: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int firstDigit = number / 10;
-int secondDigit = number % 10;
-int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;Console.WriteLine(maxDigit);
-}
+// {
+// Console.Write("Введите число из отрезка [10, 99]: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;Console.WriteLine(maxDigit);
+// }
 
+
+using System;
+class Program
+{
+static void Main()
+{
+Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 10)
+{
+Console.WriteLine(N);
+}
+else
+{
+while (N > 0)
+{
+int currentDigit = N % 10;
+N /= 10;
+if (N > 0)
+{
+Console.Write(currentDigit + ",");
+}
+else
+{
+Console.WriteLine(currentDigit);
+}
+}
+}
+}
+}
